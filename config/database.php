44efd3f987cd208b528ff6b6eabc5aea76371c18
小枫 <8353399+xfwlclub@user.noreply.gitee.com>
2022-06-21 08:31:39 +0000

    拟态UI4.0

--- config/database.php
+++ config/database.php
@@ -1,13 +1,4 @@
 <?php
-/*
- * @Author: error: git config user.name && git config user.email & please set dead value or install git
- * @Date: 2022-06-18 20:28:56
- * @LastEditors: error: git config user.name && git config user.email & please set dead value or install git
- * @LastEditTime: 2022-06-20 00:12:12
- * @FilePath: \拟态UI4.0引导页\config\database.php
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-
 return [
     // 默认使用的数据库连接配置
     'default'         => env('database.driver', 'mysql'),
